dafa9e734df70b49d1f5521786f5c6988e3f1da0
Willian-Cueva <enriquewillian2@gmail.com>
2022-08-03 08:05:01 -0500

    Creando el repositorio y disenando navbar

---
+++ src/components/ButtonLogo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ButtonLogo({onClick=()=>{},children}) {
+  return (
+    <button
+      className="transition ease-in-out delay-75 h-[34px] w-[34px] rounded-lg bg-[#EDE7F6] text-[#673AB7] flex justify-center items-center hover:bg-[#673AB7] hover:text-white"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
---
+++ src/components/MyNavbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "../assets/imgs/logo.png";
+import ButtonLogo from "./ButtonLogo";
+import { FaBars } from "react-icons/fa";
+
+export default function MyNavbar() {
+  return (
+    <header className="border p-6 font-semibold">
+      <div className="flex h-[34px] justify-between">
+        <div className="flex">
+          <span className="flex items-center mr-12">
+            <img src={logo} alt="logotipo" className="h-7 mr-2" />
+            <label className="text-xl ">Ardental</label>
+          </span>
+          <ButtonLogo>
+            <FaBars/>
+          </ButtonLogo>
+        </div>
+        <div className="flex items-center">
+          <div>sitch</div>
+          <div>Bienvenido, Willian Cueva</div>
+        </div>
+      </div>
+    </header>
+  );
+}
