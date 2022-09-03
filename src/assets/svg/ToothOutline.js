95e56394697c5e8941db2a6541a9fde075e04167
Willian-Cueva <enriquewillian2@gmail.com>
2022-09-03 00:16:57 -0500

    estilos del contorno del diente geométrico

---
+++ src/assets/svg/ToothOutline.js
@@ -0,0 +1,20 @@
+
+export default function ToothOutline({color="white",className=""}) {
+  return (
+    <div className={`${className}`}>
+      <svg
+        width="36"
+        height="9.5"
+        viewBox="0 0 104 26"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M102 25.5H2L27.5 1H77.5L102 25.5Z"
+          fill={color}
+          stroke="black"
+        />
+      </svg>
+    </div>
+  );
+}
--- src/components/Tooth.js
+++ src/components/Tooth.js
@@ -1,9 +1,16 @@
-import styles from "./styles/Tooth.module.css";
+import ToothOutline from "../assets/svg/ToothOutline";
+// import styles from "./styles/Tooth.module.css";
 export default function Tooth() {
   return (
     <div className="">
-     
-      <div></div>
+     <div className="w-[36px] h-[36px] relative">
+      <ToothOutline className="rotate-180 absolute top-0 left-0"/>
+      <ToothOutline className="-rotate-90 absolute -right-[13px] top-[13px]"/>
+      <ToothOutline className="absolute bottom-[0.5px]"/>
+      <ToothOutline className="rotate-90 absolute -left-[12.5px] top-[13px]"/>
+     </div>
+     <div></div>
+      
     </div>
   );
 }
