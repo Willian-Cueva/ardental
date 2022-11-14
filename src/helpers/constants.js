084209fbdde1eb8065b935527e69416596c1ca9b
Willian-Cueva <enriquewillian2@gmail.com>
2022-11-14 16:33:01 -0500

    v 1.2.1

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -14,10 +14,10 @@ export const dateToday = () => {
 export const BG_DARK = "bg-[#192345]"
 export const TEXT_DARK = "text-[#E2E8F0]"
 // https://ardental-backend.herokuapp.com/
-export const SERVER = "localhost:3001",
-  // export const SERVER = "ardental-backend.herokuapp.com",
-  URL = `http://${SERVER}`,
-  // URL = `https://${SERVER}`,
+// export const SERVER = "localhost:3001",
+  export const SERVER = "ardental-backend.herokuapp.com",
+  // URL = `http://${SERVER}`,
+  URL = `https://${SERVER}`,
   API = `${URL}/api`,
   PATIENTS = `${API}/patients`,
   USERS = `${API}/users`,
