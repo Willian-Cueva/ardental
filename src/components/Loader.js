03546fa86f6130bd363613311d42cd0c316caafc
Willian-Cueva <enriquewillian2@gmail.com>
2022-11-15 04:33:01 -0500

    mensajes de espera

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -5,19 +5,20 @@ export default function Loader({
   logo = true,
   width = "300px",
   height = "300px",
-  text
+  text,
 }) {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <img
-      width={width} height={height}
+        width={width}
+        height={height}
         src={logo ? img : loaderImg}
         alt="Cargando..."
-        className={`${
-          logo ? styles.loader : styles.loading
-        } object-cover`}
+        className={`${logo ? styles.loader : styles.loading} object-cover`}
       />
-      <p className="text-white">{text}</p>
+      {text&&<div className="py-2 px-3 rounded-xl mt-2">
+        <p className="text-[#2196F3] font-semibold text-xl">{text}</p>
+      </div>}
     </div>
   );
 }
